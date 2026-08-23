7f92c8b5a46a3dba4faef4a0ea8d23d3320e0012
malcolmtharris <malcolm.tharris@outlook.com>
2026-08-23 17:23:36 +0100

    Added retry round, wrong answer tracking, and score percantage

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,8 +18,13 @@ public class Main {
         words.add(new WordPair("Good evening", "Labas vakaras"));
         words.add(new WordPair("Bye", "Iki"));
 
+        Collections.shuffle(words);
+
         Scanner input = new Scanner(System.in);
         int score = 0;
+        int retryScore = 0;
+
+        ArrayList<WordPair> wrongAnswers = new ArrayList<>();
 
         for (WordPair pair : words) {
             System.out.println("Transalate: " + pair.english);
@@ -29,9 +35,30 @@ public class Main {
                 score++;
             } else  {
                 System.out.println("Wrong! The answer was: " + pair.lithuanian);
+                wrongAnswers.add(pair);
+            }
+        }
+
+        if (wrongAnswers.size() > 0) {
+            System.out.println("Time to retry the words you failed...");
+            for (WordPair pair : wrongAnswers) {
+                System.out.println("Translate: " + pair.english);
+                String answer = input.nextLine();
+
+                if (answer.equalsIgnoreCase(pair.lithuanian)) {
+                    System.out.println("Correct!");
+                    retryScore++;
+                } else {
+                    System.out.println("Wrong! The answer was: " + pair.lithuanian);
+                }
             }
         }
 
         System.out.println("Your score " + score + " out of " + words.size());
+        double percentage = (double) score / words.size() * 100;
+        System.out.println("Percentage: " + percentage + "%");
+
+        System.out.println("Retry score: " + retryScore + " out of " + wrongAnswers.size());
+
     }
 }
